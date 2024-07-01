2f12de381c6752a68767d0963c41cafc69a0fffc
Shaun Niel Ochavo <shaunniel02@gmail.com>
2024-07-01 18:39:33 +0800

    Add auth redirect to member signup

--- app/ident/member/signup/page.tsx
+++ app/ident/member/signup/page.tsx
@@ -3,7 +3,21 @@
 import SignupComponent from "@/components/Signup";
 import { signUp } from "@/utils/supabase/auth-functions";
 import { headers } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function MemberSignup() {
+export default async function MemberSignup() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user && !user?.user_metadata?.role?.includes("member")) {
+    return redirect("/ident/admin");
+  } else if (user) {
+    return redirect("/ident/member");
+  }
+
   return <SignupComponent headers={headers()} signUp={signUp} role="member" />;
 }
